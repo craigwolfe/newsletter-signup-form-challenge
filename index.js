f14024528d813794362309f1670bb65b4be130fc
Craig Wolfe <craig.s.wolfe@me.com>
2024-04-25 17:23:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const newsEl = document.getElementById("news");
 const successEl = document.getElementById("success");
+const subscribeBtn = document.getElementById("subscribe-btn");
 
 const emailIsValid = (email) => {
     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 }
 
-function hideContainer() {
-  console.log(newsEl);
-  successEl.classList.remove("success");
-  newsEl.classList.add("hide");
+// function hideContainer() {
+//   console.log(newsEl);
+//   successEl.classList.remove("success");
+//   newsEl.classList.add("hide");
+// }
+
+
+const validateForm = (event) => {
+    event.preventDefault();
+  //prevents refresh on button press
+    // If the input email address is blank or not valid
+    if (emailInput.value === '' || !emailIsValid(emailInput.value)) {
+        invalidEmailMsg.classList.remove('hidden');
+        emailInput.classList.add('invalid-input');
+        emailInput.focus();
+        return false;
+    } else {
+        newsletCardEl.classList.add('hidden');
+        successCardEl.classList.remove('hidden');
+        emailInput.classList.remove('invalid-input');
+        userEmailEl.textContent = emailInput.value;
+        return true;
+    }
 }
