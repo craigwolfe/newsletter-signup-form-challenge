95e8bd656ca9410fb652822f8c95558267d54403
Craig Wolfe <craig.s.wolfe@me.com>
2024-04-25 17:17:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const newsEl = document.getElementById("news");
 const successEl = document.getElementById("success");
 
+const emailIsValid = (email) => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
 function hideContainer() {
   console.log(newsEl);
   successEl.classList.remove("success");
